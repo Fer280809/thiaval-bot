6da9fa87a954eb04ea004f503b5b307a462cd7d9
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <fernandoagustinservinsanches@gmail.com>
2025-07-18 14:14:15 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+export const WEB_DEUDORES_URL = "https://tupagina.com/api/deudores"; // Cambia esto por tu endpoint real
+export const WEB_SYNC_INTERVAL_MIN = 10; // Cada cuántos minutos hacer sync con la web
+export const DATABASE_DIR = "./database";
+export const USERS_FILE = `${DATABASE_DIR}/users.json`;
+export const DEUDORES_FILE = `${DATABASE_DIR}/deudores.json`;
